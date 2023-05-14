88c7e3d17c1d5ddee313b518f661703511c3120c
amirtahan <amirhosin6402@gmail.com>
2023-05-14 11:51:11 +0330

    Clean cods

--- Command/DelegateCommand.cs
+++ Command/DelegateCommand.cs
@@ -5,9 +5,12 @@ namespace WiredBrainCofee.CustumrsApp.Command
 {
     class CustomDelegateCommand : ICommand
     {
+        #region Properties
         private Action<object?> _execute;
         private Func<object?, bool> _canExecute;
+        #endregion
 
+        #region Constructors
         public CustomDelegateCommand(Action<object?> execute,
             Func<object?, bool> canExecute)
         {
@@ -15,12 +18,14 @@ namespace WiredBrainCofee.CustumrsApp.Command
             _execute = execute;
             _canExecute = canExecute;
         }
+        #endregion
+
+        #region Methods
         public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
 
         public event EventHandler? CanExecuteChanged;
-
         public bool CanExecute(object? parameter) => _canExecute is null || _canExecute(parameter);
-
         public void Execute(object? parameter) => _execute?.Invoke(parameter);
+        #endregion
     }
 }
--- View/CustomersView.xaml.cs
+++ View/CustomersView.xaml.cs
@@ -10,8 +10,11 @@ namespace WiredBrainCofee.CustumrsApp.View
     /// </summary>
     public partial class CustomersView : UserControl
     {
+        #region Properties
         private CustomerViewModel _viewModel;
+        #endregion
 
+        #region Constructors
         public CustomersView()
         {
             InitializeComponent();
@@ -19,25 +22,23 @@ namespace WiredBrainCofee.CustumrsApp.View
             DataContext = _viewModel;
             Loaded += CustomersView_Loaded;
         }
+        #endregion
 
+        #region Methods
         private async void CustomersView_Loaded(object sender, RoutedEventArgs e)
         {
             await _viewModel.LoadAsync();
         }
-
         private void BtnMoveMenu_Click(object sender, RoutedEventArgs e)
         {
-            //var column = (int)customerList.GetValue(Grid.ColumnProperty);
-            //var newColumn = column == 0 ? 2 : 0;
-            //customerList.SetValue(Grid.ColumnProperty, newColumn);
             var column = Grid.GetColumn(customerList);
             var newColumn = column == 0 ? 2 : 0;
             Grid.SetColumn(customerList, newColumn);
         }
-
         private void AddCustomer_Click(object sender, RoutedEventArgs e)
         {
             _viewModel.AddCustomer();
         }
+        #endregion
     }
 }
