db8a24a4da76fc9886abdcdfe5d99fcd15fbbb5a
amirtahan <amirhosin6402@gmail.com>
2023-05-14 15:59:03 +0330

    Edit DelegateCommand

--- Command/DelegateCommand.cs
+++ Command/DelegateCommand.cs
@@ -3,16 +3,17 @@ using System.Windows.Input;
 
 namespace WiredBrainCofee.CustumrsApp.Command
 {
-    class CustomDelegateCommand : ICommand
+    public class CustomDelegateCommand : ICommand
     {
         #region Properties
+        public event EventHandler? CanExecuteChanged;
         private Action<object?> _execute;
-        private Func<object?, bool> _canExecute;
+        private Func<object?, bool>? _canExecute;
         #endregion
 
         #region Constructors
         public CustomDelegateCommand(Action<object?> execute,
-            Func<object?, bool> canExecute)
+            Func<object?, bool>? canExecute=null)
         {
             ArgumentNullException.ThrowIfNull(execute);
             _execute = execute;
@@ -22,10 +23,8 @@ namespace WiredBrainCofee.CustumrsApp.Command
 
         #region Methods
         public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
-
-        public event EventHandler? CanExecuteChanged;
-        public bool CanExecute(object? parameter) => _canExecute is null || _canExecute(parameter);
         public void Execute(object? parameter) => _execute?.Invoke(parameter);
+        public bool CanExecute(object? parameter) => _canExecute is null || _canExecute(parameter);
         #endregion
     }
 }
