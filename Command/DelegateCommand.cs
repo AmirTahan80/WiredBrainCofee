b1e9c660d67360bfd9ab2d09991dc6961bb76259
amirtahan <amirhosin6402@gmail.com>
2023-05-14 10:37:57 +0330

    Add Delegate command class for handeling the commands and add top header image...

---
+++ Command/DelegateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WiredBrainCofee.CustumrsApp.Command
+{
+    class CustomDelegateCommand : ICommand
+    {
+        private Action<object?> _execute;
+        private Func<object?, bool> _canExecute;
+
+        public CustomDelegateCommand(Action<object?> execute,
+            Func<object?, bool> canExecute)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => _canExecute is null || _canExecute(parameter);
+
+        public void Execute(object? parameter) => _execute?.Invoke(parameter);
+    }
+}
